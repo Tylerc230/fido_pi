b4d8f8e076c2e5ef01a68f656623dcb1fe7d8026
Tyler Casselman <tylerc230@gmail.com>
2013-03-13 23:25:41 -0700

    Web interface making proper calls to rpc server
    The browser client makes calls back to the robot.py controller which
    then makes calls to the rpc server

--- web_interface/controllers/robot.py
+++ web_interface/controllers/robot.py
@@ -1,2 +1,20 @@
+import xmlrpclib
+robot = xmlrpclib.ServerProxy('http://localhost:9000', allow_none=True)
 def index():
   return dict()
+
+def forward():
+  robot.DriveStraight(300)
+
+def reverse():
+  robot.DriveStraight(-300)
+
+def right():
+  robot.TurnInPlace(150, 'cw')
+
+def left():
+  robot.TurnInPlace(150, 'ccw')
+
+def stop():
+  robot.Stop()
+
