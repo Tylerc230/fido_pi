7349489066391fde744018f5551dd143a8352b3c
Tyler Casselman <tylerc230@gmail.com>
2013-03-14 00:40:52 -0700

    Run start as sudo sh ./start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-sudo python ./xmlrpc_server.py &
-cd ~/tools/web2py/
-sudo python web2py.py -i 192.168.2.2 -p 80 -a 'tyler23' -v &
+python ./xmlrpc_server.py &
+cd /home/tcasselman/tools/web2py/
+python web2py.py -i 192.168.2.2 -p 80 -a 'tyler23' -v &
 cd -
 
