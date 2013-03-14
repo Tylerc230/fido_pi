334f830dfa584b28718320656267d5f7b84de1bf
Tyler Casselman <tylerc230@gmail.com>
2013-03-13 23:26:33 -0700

    Create one script to start web server and rpc server

---
+++ start.sh
@@ -0,0 +1,5 @@
+sudo python ./xmlrpc_server.py &
+cd ~/tools/web2py/
+sudo python web2py.py -i 192.168.2.2 -p 80 -a 'tyler23' -v &
+cd -
+
--- start_server.sh
+++
@@ -1,4 +0,0 @@
-cd ~/tools/web2py/
-sudo python web2py.py -i 192.168.2.2 -p 80 -a 'tyler23' -v
-cd -
-
