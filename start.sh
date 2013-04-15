ce0d3879d192262dab46d2b603e9a827577b0e7d
Tyler Casselman <tylerc230@gmail.com>
2013-04-15 16:18:09 -0700

    Serve web page via wireless interface

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 python ./xmlrpc_server.py &
 cd /home/tcasselman/tools/web2py/
-python web2py.py -i 192.168.2.2 -p 80 -a 'tyler23' -v &
+python web2py.py -i 192.168.1.7 -p 80 -a 'tyler23' -v &
 cd -
 
