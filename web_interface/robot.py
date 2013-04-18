8bd0cc3041239f9d504f3c2c815f0ffdd6fd93b0
Tyler Casselman <tylerc230@gmail.com>
2013-04-18 16:05:29 -0700

    Use flask instead of web2py
    Flask is a much more lightweight framework and more suited to pi.

---
+++ web_interface/robot.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import render_template
+import xmlrpclib
+
+app = Flask(__name__)
+robot = xmlrpclib.ServerProxy('http://localhost:9000', allow_none=True)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/robot/forward')
+def forward():
+  robot.DriveStraight(150)
+  return ""
+
+@app.route('/robot/reverse')
+def reverse():
+  robot.DriveStraight(-150)
+  return ""
+
+@app.route('/robot/right')
+def right():
+  robot.TurnInPlace(150, 'cw')
+  return ""
+
+@app.route('/robot/left')
+def left():
+  robot.TurnInPlace(150, 'ccw')
+  return ""
+
+@app.route('/robot/stop')
+def stop():
+  robot.Stop()
+  return ""
+
+if __name__ == "__main__":
+  app.run(debug=True, host='192.168.1.7', port=80)
