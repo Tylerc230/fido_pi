f3ee85d0d789057841cdcc2240fe3dd7435b8a98
Tyler Casselman <tylerc230@gmail.com>
2013-06-15 11:21:25 -0700

    Start using flash instead of web2py
    We also write the current pid to a file.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
+$PID = $$
+echo $PID > robot.pid
 python ./xmlrpc_server.py &
-cd /home/tcasselman/tools/web2py/
-python web2py.py -i 192.168.1.7 -p 80 -a 'tyler23' -v &
-cd -
+python web_interface/robot.py
 
